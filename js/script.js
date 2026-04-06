78eca0113b8bc5f10f30bfcbc1771cd520fa57b1
KetyBezerra <ketellyn.7636712@aluno.mg.gov.br>
2026-04-06 16:07:46 +0000

    aula 6-3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let likeCount = 0;
 let curtido = false; // flag booleana
 
+let deslikeCount = 0;
+let descurtido = false; // flag boolena
+
 function curtir() {
 
  if (curtido == false){
@@ -13,7 +16,22 @@ function curtir() {
   document.getElementById("likeCount").innerText = likeCount;
 }
 
+}
+
+document.getElementById("likeBtn").addEventListener("click", curtir);
+
+function descurtir() {
+ if (descurtido == false){
+     deslikeCount++;
+     descurtido = true;
+    document.getElementById("deslikeCount").innerText = deslikeCount;
+  }else{
+    deslikeCount--;
+    descurtido = false;
+    document.getElementById("deslikeCount").innerText = deslikeCount;
+  }
 
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("deslikeBtn").addEventListener("click", descurtir);
